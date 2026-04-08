fd584da1745dc52ee0930dc6618b0ec4739ce480
Sergio Garcia Vargas <petezajot@hotmail.com>
2026-04-08 15:12:02 -0600

    Refactor ___VARIABLE_sceneName___ViewModel for model updates

--- ___FILEBASENAME___ViewModel.swift
+++ ___FILEBASENAME___ViewModel.swift
@@ -1,17 +1,17 @@
 //___FILEHEADER___
 
 import Foundation
+import Combine
 
 protocol ___VARIABLE_sceneName___Protocol {
     func increment()
 }
 
 final class ___VARIABLE_sceneName___ViewModel: ObservableObject,  ___VARIABLE_sceneName___Protocol {
-    @Published var model: ___VARIABLE_sceneName___Model?
-    var num = 0
+    @Published var model: ___VARIABLE_sceneName___Model = ___VARIABLE_sceneName___Model(number: 0)
     
     func increment() {
-        num += 1
-        model = ___VARIABLE_sceneName___Model(number: num)
+        model.number += 1
+        model = ___VARIABLE_sceneName___Model(number: model.number)
     }
 }
